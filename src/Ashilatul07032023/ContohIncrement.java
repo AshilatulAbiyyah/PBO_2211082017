7737b9e6b05f0c1aa3bae5532a1143bc2dd92a4b
AshilatulAbiyyah <127171113+AshilatulAbiyyah@users.noreply.github.com>
2023-03-08 11:39:15 +0700

    Initial commit

---
+++ src/Ashilatul07032023/ContohIncrement.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ashilatul07032023;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class ContohIncrement {
+    public static void main(String[] args){
+        
+        int i = 10;
+        int j = 3;
+        int k = 0;
+        
+        k = ++j + i;
+        k = j++ + i:
+        
+        System.out.println(k);
+        System.out.println(i);
+        System.out.println(j++);
+    }
+    
+}
