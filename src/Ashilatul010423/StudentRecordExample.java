d3f2856d41a1cd5038732883a6a3fbf250a1486e
MinurfiZ31 <minurfi31@gmail.com>
2023-04-11 15:32:08 +0700

    PBO_2211082017

--- src/Ashilatul010423/StudentRecordExample.java
+++ src/Ashilatul010423/StudentRecordExample.java
@@ -6,40 +6,53 @@ package Ashilatul010423;
 
 /**
  *
- * @author User
+ * @author Ashilatul
  */
 public class StudentRecordExample {
     public static void main(String[] args){
-        StudentRecord ani = new StudentRecord();
+        StudentRecord anna = new StudentRecord();
         StudentRecord cris = new StudentRecord();
         StudentRecord ali = new StudentRecord("ali");
         
         //isi data
-        ani.setName("Ani");
-        ani.setAddress("Padang");
-        ani.setAge(20);
+        anna.setName("Anna");
+        anna.setAddress("Padang");
+        anna.setAge(18);
+        anna.setmathGrade(80);
+        anna.setenglishGrade(98);
         
-        cris.setName("Cris");
+        cris.setName("Kris");
         cris.setAddress("Bukittinggi");
-        cris.setAge(20);
+        cris.setAge("20");
         
-        //menampilkan data
-        System.out.println("Data Objek Ani");
-        System.out.println("Nama        :" +ani.getName());
-        System.out.println("Alamat      :" +ani.getAddress());
-        System.out.println("Umur        :" +ani.getAge());
+        ali.setName("Ali");
+        ali.setAddress("Bandung");
+        ali.setAge("20");
+        
+        //menampilkan
+        System.out.println("Data Objek Anna");
+        System.out.println("Nama    :" + anna.getName());
+        System.out.println("Alamat  :" + anna.getAddress());
+        System.out.println("Umur    :" + anna.getAge()); 
+        System.out.println("Math Grade  :" + anna.getmathGrade());
+        System.out.println("English Grade   :" + anna.getenglishGrade()); 
+        System.out.println("Science Grade :" + anna.getscienceGrade());
+        System.out.println("Rata-rata  :" + anna.getAverage());
+        System.out.println("Nilai Huruf  :" + anna.getnilaiHuruf()); 
+        
+        System.out.println("");
         
         //memanggil method print
-        System.out.println("Data Objek Cris");
+        System.out.println("Data Objek Kris");
         cris.print("");
-        cris.print(70, 80, 90);
+        cris.print(70,80,90);
         
         System.out.println("Data Objek Ali");
+        System.out.println("");
         ali.print("");
         
-        
         //menampilkan jumlah siswa
-        System.out.println("Count = "+StudentRecord.getStudentCount());
-        
+        System.out.println("");
+        System.out.println("Count =" +StudentRecord.getStudentCount());     
     }
 }
---
+++ src/Ashilatul080423/Person.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ashilatul080423;
+
+/**
+ *
+ * @author Ashilatul
+ */
+public class Person {
+    
+    protected String name;
+    protected String address;
+    
+    public Person(){
+        System.out.println("Inside Person :Contructor");
+    }
+    
+    public Person(String name,String address){
+        this.name = name;
+        this.address = address;
+    }
+    
+    public String getName(){
+        System.out.println("Person :getName");
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getAddress(){
+       return address;    
+    }
+    
+    public void setAddress(String address){
+        this.address = address;
+    }
+}
